98d5e222327db69d8a3dd0559af75bf1e33e3bf7
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-09-21 12:51:58 -0700

    feat: build personal introduction CLI for Day 1

---
+++ personal-intoduction/src/PersonalIntroductionCLI.java
@@ -0,0 +1,10 @@
+public class PersonalIntroductionCLI {
+    public static void main(String[] args) throws Exception {
+        System.out.println("====================================");
+        System.out.println("WELCOME TO MY JAVA MASTERY JOURNEY");
+        System.out.println("====================================");
+        System.out.println("Developer: Caleb");
+        System.out.println("Goal: 60 Days of Core Java & OOP");
+        System.out.println("====================================");
+    }
+}
